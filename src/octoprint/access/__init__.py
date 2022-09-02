e0ed8a806f0dc2ae6342421aae82c086df2c739b
bjarne-dietrich <bjarne.dietrich@gmx.de>
2022-09-02 20:39:48 +0100

    Initial commit of Octoprint-Mmu2s

--- src/octoprint/access/__init__.py
+++
@@ -1,4 +0,0 @@
-ADMIN_GROUP = "admins"
-USER_GROUP = "users"
-GUEST_GROUP = "guests"
-READONLY_GROUP = "readonly"
--- tests/timelapse/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Unit tests for ``octoprint.timelapse``.
-"""
-
-__license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
-__copyright__ = "Copyright (C) 2016 The OctoPrint Project - Released under terms of the AGPLv3 License"
